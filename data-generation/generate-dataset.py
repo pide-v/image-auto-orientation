44cca5157336bbfde9f09712ef61ef3eb4efcde3
pidez <lorenzo.iaco@hotmail.com>
2025-02-06 16:46:18 +0100

    Handled exception when directory already exists in generate-dataset.py

--- data-generation/generate-dataset.py
+++ data-generation/generate-dataset.py
@@ -3,22 +3,30 @@ from PIL import Image
 import os
 import csv
 
+#path must be a folder containing only N subfolders. Each subfolder's name represents the
+#class of the images that it contains. 
 
-path = 'test-images/imagenette2-160/train'
-test_path = 'test-images/test'
+#dest_path specifies where to save the 4 folders a_0, a_90, a_180, a_270 for 
+#each subfolder a.
 
-classes = [f for f in os.listdir(test_path) if not f.startswith('.')]
+path = 'test-images/images'
+dest_path = 'test-images/mock-dataset'
+
+classes = [f for f in os.listdir(path) if not f.startswith('.')]
 print(f'Found {len(classes)} classes: {classes}')
 
 for folder in classes:
 	print(f'### Processing images of class {folder} ###')
-	for rot in [90, 180, 270]:
-		os.mkdir(f'{test_path}/{folder}_{rot}')
-		for im in os.listdir(f'{test_path}/{folder}'):
-			img = Image.open(f'{test_path}/{folder}/{im}')
+	for rot in [0, 90, 180, 270]:
+		try:
+			os.mkdir(f'{dest_path}/{folder}_{rot}')
+		except FileExistsError:
+			print(f'generate-dataset.py: Directory {dest_path}/{folder}_{rot} already exists. Ignoring creation.')
+		for im in os.listdir(f'{path}/{folder}'):
+			img = Image.open(f'{path}/{folder}/{im}')
 			rot_img = img.rotate(rot)
 			print(f'Saving image {im} at {rot} degrees')
-			rot_img.save(f'{test_path}/{folder}_{rot}/{im[:-5]}_{rot}.JPEG')
+			rot_img.save(f'{dest_path}/{folder}_{rot}/{im[:-5]}_{rot}.JPEG')
 
 		
 
