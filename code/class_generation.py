9973a539c298c9a756bd8d5f12584e2ad835fc3c
Stefano Gualtieri <s.gualtieri5@campus.unimib.it>
2025-02-10 19:43:44 +0100

    Added dataset_generation.py file, data_generation -> class_generation.py

--- code/class_generation.py
+++ code/class_generation.py
@@ -1,5 +1,9 @@
 import utils as utils
 
+'''
+Run this file to generate dataset in the folder mock-dataset
+'''
+
 img_path = "Desktop/data-generation/images"
 dest_path = "Desktop/data-generation/mock-dataset"
 
---
+++ code/dataset_generation.py
@@ -0,0 +1,5 @@
+import utils as utils
+
+'''
+Here generate dataset and save two numpy array as x and y
+'''
