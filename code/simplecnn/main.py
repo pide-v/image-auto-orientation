3270f9ceab2e858ae0c758fb2280e44bde080698
Stefano Gualtieri <s.gualtieri5@campus.unimib.it>
2025-02-11 19:58:04 +0100

    added model choiche in main.py

--- code/simplecnn/main.py
+++ code/simplecnn/main.py
@@ -1,4 +1,13 @@
 import models as models
 
-model_01 = models.build_model_01((426, 320, 3), 4)
-model_02 = models.build_model_02((426, 320, 3), 4)
+"""
+Here you can choose which model to train from the list
+"""
+
+print("Models list: \nmodel01 \nmodel02 \n")
+model = input("Please insert to model you would like to train: ")
+
+if model == "model01":
+    model_01 = models.build_model_01((426, 320, 3), 4)
+elif model == "model02":
+    model_02 = models.build_model_02((426, 320, 3), 4)
