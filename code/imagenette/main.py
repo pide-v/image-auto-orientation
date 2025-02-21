710657f17d9d29308bc5819c405b10c059374ddc
pidez <lorenzo.iaco@hotmail.com>
2025-02-21 19:31:38 +0100

    ripulito codice

---
+++ code/imagenette/main.py
@@ -0,0 +1,88 @@
+import sys
+sys.path.append('/home/pide/aml/image-auto-orientation/code/')
+import utils as ut
+
+import models as models
+
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
+
+import time
+
+dataset = 'street-view'
+
+train_path = f'/home/pide/aml/image-auto-orientation/datasets/{dataset}/train'
+test_path = f'/home/pide/aml/image-auto-orientation/datasets/{dataset}/test'
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.15
+)
+
+train_generator = train_datagen.flow_from_directory(
+    train_path,
+    target_size=(224, 224),
+    batch_size=16,
+    class_mode="binary",
+    subset="training"
+)
+
+val_generator = train_datagen.flow_from_directory(
+    train_path,
+    target_size=(224, 224),
+    batch_size=16,
+    class_mode="binary",
+    subset="validation"
+)
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=4,
+    restore_best_weights=True
+)
+
+optimizer = 'adam'
+loss = "binary_crossentropy"
+
+model = models.build_model_02((224,224, 3), 1)
+
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+start = time.time()
+history = model.fit(train_generator, validation_data=val_generator, epochs=30, callbacks=[early_stopping])
+end = time.time()
+
+total_time = end - start
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_path,
+    target_size=(224, 224),
+    batch_size=16,
+    class_mode="binary",
+    shuffle=False
+)
+
+test_loss, test_acc = model.evaluate(test_generator, verbose=1)
+
+print(f"Test Loss: {test_loss:.4f}")
+print(f"Test Accuracy: {test_acc:.4f}")
+
+ut.save_model_and_metrics(model, model.count_params(), total_time, history, test_acc, dataset, '../../trained-models', 'model03-streetview')
---
+++ code/imagenette/models.py
@@ -0,0 +1,119 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # set tf log to error only
+
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+from tensorflow.keras import layers, Sequential
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
+from tensorflow.keras.regularizers import l2
+
+
+def plot_loss(history):
+  x_plot = list(range(1,len(history.history["loss"])+1))
+  plt.figure()
+  plt.title("Loss")
+  plt.xlabel('Epochs')
+  plt.ylabel('Loss')
+  plt.plot(x_plot, history.history['loss'])
+  plt.plot(x_plot, history.history['val_loss'])
+  plt.legend(['Training', 'Validation'])
+  plt.show()
+
+def plot_accuracy(history):
+  x_plot = list(range(1,len(history.history["accuracy"])+1))
+  plt.figure()
+  plt.title("Accuracy")
+  plt.xlabel('Epochs')
+  plt.ylabel('Accuracy')
+  plt.plot(x_plot, history.history['accuracy'])
+  plt.plot(x_plot, history.history['val_accuracy'])
+  plt.legend(['Training', 'Validation'])
+  plt.show()
+
+
+# first simple model
+def build_model_01(input_shape, num_classes):
+    model = Sequential()
+    model.add(Input(shape=input_shape))
+
+    model.add(Conv2D(16, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Flatten())
+
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(num_classes, activation='sigmoid'))
+
+    model.summary()
+
+    return model
+
+
+def build_model_02(input_shape, num_classes):
+    model=Sequential()
+    model.add(Input(shape=input_shape))
+    
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3)))
+    
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu', kernel_regularizer=l2(0.0001)))
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3)))
+    
+    model.add(Conv2D(128, kernel_size=(3,3), activation='relu', kernel_regularizer=l2(0.0001)))
+    model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3,3)))  
+    model.add(Flatten())
+    
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.25))
+    
+    model.add(Dense(32, activation='relu'))
+    
+    model.add(Dropout(0.25))
+    model.add(Dense(num_classes, activation='sigmoid'))
+
+    model.summary()
+
+    return model
+
+
+def build_model_03(input_shape, num_classes):
+    model=Sequential()
+    model.add(Input(shape=input_shape))
+
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', kernel_regularizer=l2(0.001)))
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3,3), activation='relu', kernel_regularizer=l2(0.001)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Flatten())
+
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(num_classes, activation='sigmoid'))
+
+    model.summary()
+    return model
