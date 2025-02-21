d0bcc305103172d630217843f9b9f44fadcf1de5
pidez <lorenzo.iaco@hotmail.com>
2025-02-21 12:18:17 +0100

    ripulito codice

--- code/cifar10/dataset_generator.py
+++ code/cifar10/dataset_generator.py
@@ -3,7 +3,6 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 
-# Imposta i percorsi
 base_path = os.path.expanduser("~/aml/image-auto-orientation/cifar10")
 
 normal_path = os.path.join(base_path, 'normal')
@@ -14,24 +13,19 @@ os.makedirs(rotated_path, exist_ok=True)
 
 rotation_angles = [90, 180, 270]
 
-# Carica il dataset MNIST
 (x_train, _), (x_test, _) = tf.keras.datasets.cifar10.load_data()
 
-# Combina train e test
 dataset = np.concatenate((x_train, x_test), axis=0)
 num_images = len(dataset)
 
-# Dividi il dataset in due metà
 half = num_images // 2
 normal_images = dataset[:half]
 rotated_images = dataset[half:]
 
-# Salva le immagini normali
 for i, img_array in enumerate(normal_images):
     img = Image.fromarray(img_array)
     img.save(os.path.join(normal_path, f'img_{i}.png'), 'PNG')
 
-# Salva le immagini ruotate
 for i, img_array in enumerate(rotated_images):
     img = Image.fromarray(img_array)
     rotation_angle = rotation_angles[i % len(rotation_angles)]
